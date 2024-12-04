de8f14ea498d904106162ffc6112cb329e948cff
ing-enuarlara <enuar2110@gmail.com>
2024-12-04 15:00:30 +0100

    Se añade verificacion de caracteres especiales en conexin para permitir tildes

--- panelcontrol/modelo/conexion.php
+++ panelcontrol/modelo/conexion.php
@@ -3,5 +3,11 @@ $conexion = mysqli_connect("jmequipos.com","jmequipo_admin","fuVwyXWMXxK5");
 //if($conexion){echo "conexión exitosa";}else{echo "No hubo conexion"; exit();}
 mysqli_select_db($conexion,"jmequipo_panelcontrol");
 
+if (!mysqli_set_charset($conexion, "utf8mb4")) 
+{
+    printf("Error cargando el conjunto de caracteres utf8mb4: %s\n", mysqli_error($link));
+    exit();
+}
+
 $bdJM = 'jmequipo_jm';
 ?>
--- panelcontrol/modelo/conexionjm.php
+++ panelcontrol/modelo/conexionjm.php
@@ -1,4 +1,10 @@
 <?php
 $conexionjm = mysqli_connect("jmequipos.com","jmequipo_admin","fuVwyXWMXxK5");
-mysqli_select_db($conexionjm,"jmequipo_jm")
+mysqli_select_db($conexionjm,"jmequipo_jm");
+
+if (!mysqli_set_charset($conexionjm, "utf8mb4")) 
+{
+    printf("Error cargando el conjunto de caracteres utf8mb4: %s\n", mysqli_error($link));
+    exit();
+}
 ?>
